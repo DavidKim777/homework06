277d34250ce322535b105e81217d20daf6e83966
moses <DavidKim777>
2025-02-07 14:37:49 +0200

    homework06

--- test/homework06_SUITE.erl
+++ test/homework06_SUITE.erl
@@ -5,40 +5,40 @@
 -include_lib("common_test/include/ct.hrl").
 
 -export([all/0,
-  init_per_suite/1,
-  end_per_suite/1,
-  init_per_testcase/2,
-  end_per_testcase/2]).
+    init_per_suite/1,
+    end_per_suite/1,
+    init_per_testcase/2,
+    end_per_testcase/2]).
 
 all() ->
-  [create_table, insert_and_lookup, insert_with_ttl].
+    [create_table, insert_and_lookup, insert_with_ttl].
 
 init_per_suite(Config) ->
-  {ok, _} = homework06:start_link(),
-  Config.
+    {ok, _} = homework06:start_link(),
+    Config.
 
 end_per_suite(_Config) ->
   homework06:stop(),
   ok.
 
 init_per_testcase(_TestCase, Config) ->
-  Config.
+    Config.
 
 
 end_per_testcase(_TestCase, _Config) ->
-  ok.
+    ok.
 
 create_table(_Config) ->
-  ?assertEqual(ok, homework06:create(my_table)).
+    ?assertEqual(ok, homework06:create(my_table)).
 
 insert_and_lookup(_Config) ->
-  homework06:create(my_table),
-  homework06:insert(my_table, key1, value1),
-  ?assertEqual(value1, homework06:lookup(my_table, key1)).
+    homework06:create(my_table),
+    homework06:insert(my_table, key1, value1),
+    ?assertEqual(value1, homework06:lookup(my_table, key1)).
 
 insert_with_ttl(_Config) ->
-  homework06:create(my_table),
-  homework06:insert(my_table, key2, value2, 1),
-  ?assertEqual(value2, homework06:lookup(my_table, key2)),
-  timer:sleep(2000),
-  ?assertEqual(undefined, homework06:lookup(my_table, key2)).
+    homework06:create(my_table),
+    homework06:insert(my_table, key2, value2, 1),
+    ?assertEqual(value2, homework06:lookup(my_table, key2)),
+    timer:sleep(2000),
+    ?assertEqual(undefined, homework06:lookup(my_table, key2)).
